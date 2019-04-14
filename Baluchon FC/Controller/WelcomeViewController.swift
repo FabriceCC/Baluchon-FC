c5cec8ed0447ee52e1e6d58a9ea534ea4a8f1491
fabrice CONCHOU <fabrice.conchou@laposte.net>
2019-04-14 15:46:16 +0200

    With all views

---
+++ Baluchon FC/Controller/WelcomeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  WelcomeViewController.swift
+//  Baluchon FC
+//
+//  Created by utilisateur on 14/04/2019.
+//  Copyright © 2019 Fabrice. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var welcomeButton: UIButton!
+    
+// corner.radius
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func WelcomeAction() {
+    }
+    
+}
