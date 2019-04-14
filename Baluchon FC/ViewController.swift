363f12a7de1b0313cd868d915b2ed1e6be444bb9
fabrice CONCHOU <fabrice.conchou@laposte.net>
2019-04-14 10:58:05 +0200

    Initial Commit

---
+++ Baluchon FC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baluchon FC
+//
+//  Created by utilisateur on 14/04/2019.
+//  Copyright © 2019 Fabrice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
